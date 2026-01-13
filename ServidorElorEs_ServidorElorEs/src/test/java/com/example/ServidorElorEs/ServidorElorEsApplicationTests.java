a66cbf71cebb9b3543d462e96b78af2a36d427ce
Anaitz Greño <anaitz.grenopa@elorrieta-errekamari.com>
2026-01-13 08:23:19 +0100

    Springboot

---
+++ ServidorElorEs_ServidorElorEs/src/test/java/com/example/ServidorElorEs/ServidorElorEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServidorElorEs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorElorEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
